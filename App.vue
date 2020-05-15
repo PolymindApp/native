cd2d0e51e7b431945b3fe4eb2242c29327f02ceb
bastiengoulet <bastien.goulet27@outlook.com>
2020-05-15 13:49:15 -0500

    My Vue Native project just to test

---
+++ App.vue
@@ -0,0 +1,23 @@
+<template>
+	<view class="container">
+		<StatusBar background-color="blue" bar-style="light-content" />
+		<web-view :source="{uri: 'https://client.dev.polymind.app'}" />
+	</view>
+</template>
+
+<script>
+	import { WebView } from 'react-native-webview';
+
+	export default {
+		components: {
+			'web-view': WebView
+		}
+	}
+</script>
+
+<style>
+	.container {
+		background-color: white;
+		flex: 1;
+	}
+</style>
